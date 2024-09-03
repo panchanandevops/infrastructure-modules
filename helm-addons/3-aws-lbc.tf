5565db0d15a2dfadfc8514d3cb3e76fb66a0a8c1
Panchanan Panigrahi <panchanandevops@gmail.com>
2024-09-03 19:46:20 +0530

    helm-v0.0.2 is created

--- helm-addons/3-aws-lbc.tf
+++ helm-addons/3-aws-lbc.tf
@@ -19,7 +19,7 @@ resource "aws_iam_role" "aws_lbc" {
   count = var.enable_aws_lbc ? 1 : 0
 
   name               = "${var.eks_name}-aws-lbc"
-  assume_role_policy = data.aws_iam_policy_document.aws_lbc.json
+  assume_role_policy = data.aws_iam_policy_document.aws_lbc[0].json
 }
 
 resource "aws_iam_policy" "aws_lbc" {
@@ -32,8 +32,8 @@ resource "aws_iam_policy" "aws_lbc" {
 resource "aws_iam_role_policy_attachment" "aws_lbc" {
   count = var.enable_aws_lbc ? 1 : 0
 
-  policy_arn = aws_iam_policy.aws_lbc.arn
-  role       = aws_iam_role.aws_lbc.name
+  policy_arn = aws_iam_policy.aws_lbc[0].arn
+  role       = aws_iam_role.aws_lbc[0].name
 }
 
 resource "aws_eks_pod_identity_association" "aws_lbc" {
@@ -42,7 +42,7 @@ resource "aws_eks_pod_identity_association" "aws_lbc" {
   cluster_name    = var.eks_name
   namespace       = "kube-system"
   service_account = "aws-load-balancer-controller"
-  role_arn        = aws_iam_role.aws_lbc.arn
+  role_arn        = aws_iam_role.aws_lbc[0].arn
 }
 
 resource "helm_release" "aws_lbc" {
